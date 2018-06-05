327c5a7e3b352da9586145e9fedb66a83a2d22e5
Lena Stresova <lenastres@gmail.com>
2018-06-05 05:45:59 +0300

    Add responsive styles

--- js/init.js
+++ js/init.js
@@ -3,10 +3,17 @@ $( function() {
     $( ".services-tab" ).tabs();
     // init portfolio tabs
     $( ".portfolio-tab" ).tabs();
-    //init reviews carousel
-    $('.reviews-carousel').slick({
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 5000
+    // init reviews carousel     
+    $(".reviews-carousel").slick({        
+        dots: true,        
+        autoplay: true,        
+        autoplaySpeed: 5000,        
+        prevArrow: '<div class="slick-prev slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-left"></i></div></div>',        
+        nextArrow: '<div class="slick-next slick-arrow"><div class="carousel-button"><i class="fa fa-chevron-right"></i></div></div>'   
+    });
+    // init slicknav
+    $(".header-nav").slicknav({
+        appendTo: '#header .header-container',
+        label: ''
     });
 } );
